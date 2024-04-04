a5a786b6ba5abbb46e50144ec725f62c460dedf2
Thanapat <earthza3019@gmail.com>
2024-04-04 13:33:42 +0700

    Initial Commit

---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by นายธนภัทร สาระธรรม on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
